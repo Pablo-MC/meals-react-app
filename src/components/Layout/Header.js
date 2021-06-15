44512832778a8caa84c8e2651a3828974a587a61
Pablo <cruzpablomartin@gmail.com>
2021-06-15 13:59:56 -0300

    Added Meals Component

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,17 +1,17 @@
 import { Fragment } from 'react';
 
-import styles from './Header.module.css';
+import classes from './Header.module.css';
 import mealsImage from '../../assets/meals.jpg';
 import HeaderCartButton from './HeaderCartButton';
 
 const Header = () => {
   return (
     <Fragment>
-      <header className={styles.header}>
+      <header className={classes.header}>
         <h1>ReactMeals</h1>
         <HeaderCartButton />
       </header>
-      <div className={styles['main-image']}>
+      <div className={classes['main-image']}>
         <img src={mealsImage} alt="A table full of delicious food!" />
       </div>
     </Fragment>
--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,14 +1,14 @@
-import styles from './HeaderCartButton.module.css';
+import classes from './HeaderCartButton.module.css';
 import CartIcon from '../Cart/CartIcon';
 
 const HeaderCartButton = () => {
   return (
-    <button className={styles.button}>
-      <span className={styles.icon}>
+    <button className={classes.button}>
+      <span className={classes.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={styles.badge}>3</span>
+      <span className={classes.badge}>3</span>
     </button>
   );
 }
